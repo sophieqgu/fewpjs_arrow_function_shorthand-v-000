8fd34db8afcccde016617511c07d5d9f198465ff
Sophie Gu <sophieqgu@gmail.com>
2021-01-25 03:13:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const divide = (x, y) => { x / y };
-const square = x => { x ** 2 };
-const add = (x, y) => { x + y };
+const divide = (x, y) => x / y;
+const square = x => x ** 2;
+const add = (x, y) => x + y;
